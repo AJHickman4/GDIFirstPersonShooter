c4a51533349d1d7bdc32e5c4e6b195c2daadc076
CollinHedges <Chedges@Student.Fullsail.edu>
2024-03-10 14:51:26 -0400

    FOV while sprinting

--- GDIGroupFPS/Assets/Scripts/cameraController.cs
+++ GDIGroupFPS/Assets/Scripts/cameraController.cs
@@ -8,6 +8,11 @@ public class cameraController : MonoBehaviour
     [SerializeField] int sensitivity;
     [SerializeField] int lockVertMin, LockVertMax; //usually 90 and -90 for scoape
     [SerializeField] bool invertY;
+    [SerializeField] Camera mainCamera; //references main camera
+    [SerializeField] float normalFOV = 60f;
+    [SerializeField] float sprintingFOV = 75f;
+    [SerializeField] float FOVTransition = 10f; //need this transition time for the lerp function below. 
+
 
     float rotX; //how we get the rotation on the x axis. 
 
@@ -18,7 +23,10 @@ public class cameraController : MonoBehaviour
         Cursor.lockState = CursorLockMode.Locked; //cannot leave screen space while hidden.
         transform.forward = transform.parent.forward;//get camera to face forward right out of the gate at start. 
 
+
+
     }
+
 
     // Update is called once per frame
     void Update()
@@ -42,5 +50,16 @@ public class cameraController : MonoBehaviour
         //rotate the player on the y-axis
         transform.parent.Rotate(Vector3.up * mouseX);
 
+
+    //adjust FOV while sprinting..
+    if (Input.GetKey(KeyCode.LeftShift))
+        {
+            mainCamera.fieldOfView = Mathf.Lerp(mainCamera.fieldOfView, sprintingFOV, FOVTransition * Time.deltaTime); //if shift is pressed make FOV become number
+        }
+        else
+        {
+            mainCamera.fieldOfView = Mathf.Lerp(mainCamera.fieldOfView, normalFOV, FOVTransition * Time.deltaTime); //reverts FOV back after sprinting / sets norn speed 
+        }
+        
     }
 }
