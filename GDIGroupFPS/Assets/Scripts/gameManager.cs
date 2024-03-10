794e298a45895f1711f4a00d1137a8d95b51f6f5
amabunada <137794655+amabunada@users.noreply.github.com>
2024-03-10 13:44:21 -0400

    Ali - Added Personal Texture Folder

--- GDIGroupFPS/Assets/Scripts/gameManager.cs
+++ GDIGroupFPS/Assets/Scripts/gameManager.cs
@@ -1,18 +1,69 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class gameManager : MonoBehaviour
 {
+    public static gameManager instance;
+
+    [SerializeField] GameObject menuActive;
+    [SerializeField] GameObject menuPause;
+    [SerializeField] GameObject menuWin;
+    [SerializeField] GameObject menuLose;
+
+    public GameObject player;
+    public playerController playerScript;
+
+    public bool isPaused;
+    float timeScaleOrig;
+    int enemyCount;
+
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
-        
+        instance = this;
+        player = GameObject.FindWithTag("Player");
+        playerScript = player.GetComponent<playerController>();
+        timeScaleOrig = Time.timeScale;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetButtonDown("Cancel") && menuActive == null)
+        {
+            statePaused();
+            menuActive = menuPause;
+            menuActive.SetActive(isPaused);
+        }
+    }
+
+    public void statePaused()
+    {
+        isPaused = !isPaused;
+        Time.timeScale = 0;
+        Cursor.visible = true;
+        Cursor.lockState = CursorLockMode.Confined;
+    }
+    public void stateUnPaused()
+    {
+        isPaused = !isPaused;
+        Time.timeScale = timeScaleOrig;
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+        menuActive.SetActive(isPaused);
+        menuActive = null;
+    }
+    public void updateGameGoal(int amount)
+    {
+        enemyCount += amount;
+
+        if (enemyCount <= 0)
+        {
+            statePaused();
+            menuActive = menuWin;
+            menuActive.SetActive(true);
+        }
     }
 }
