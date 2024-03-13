8f28038d5c38a159a2bc55197f19e86a107150c6
AJHickman4 <ajhickman@student.fullsail.edu>
2024-03-12 21:55:54 -0500

    Working on Enemy Pathing

--- GDIGroupFPS/Assets/Scripts/enemyAI.cs
+++ GDIGroupFPS/Assets/Scripts/enemyAI.cs
@@ -8,24 +8,111 @@ public class enemyAI : MonoBehaviour, IDamage
     [Header("----- Stats -----")]
     [Range (5, 50)] [SerializeField] int HP;
     [SerializeField] Renderer model;
-    [SerializeField] NavMeshAgent agent;
 
     [Header("----- Gun Stats -----")]
     [SerializeField] GameObject bullet;
     [SerializeField] float shootRate;
+    [SerializeField] Transform BulletSpawn;
 
     private Animator animator;
 
-    void Start()
-    {
+    [Header("----- Pathing -----")]
+    public NavMeshAgent agent;
+    public Transform player;
+    public LayerMask whatIsGround, whatIsPlayer;
 
+    
+
+    //Patroling
+    public Vector3 walkPoint;
+    bool walkPointSet;
+    public float walkPointRange;
+
+    //Chasing
+    public float timeBetweenAttacks;
+    bool alreadyAttacked;
+
+    //States
+    public float sightRange, attackRange;
+    public bool playerInSightRange, playerInAttackRange;
+
+    private void Awake()
+    {
+        player = GameObject.Find("Player").transform;
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    private void Update()
+    {
+        //Check for sight and attack range
+        playerInSightRange = Physics.CheckSphere(transform.position, sightRange, whatIsPlayer);
+        playerInAttackRange = Physics.CheckSphere(transform.position, attackRange, whatIsPlayer);
+
+        if (!playerInSightRange && !playerInAttackRange) Patroling();
+        if (playerInSightRange && !playerInAttackRange) ChasePlayer();
+        if (playerInSightRange && playerInAttackRange) AttackPlayer();
     }
 
 
-
-    void Update()
+    private void Patroling()
     {
+        if (!walkPointSet) SearchWalkPoint();
 
+        if (walkPointSet)
+        {
+            agent.SetDestination(walkPoint);
+        }
+
+        Vector3 distanceToWalkPoint = transform.position - walkPoint;
+
+        //Walkpoint reached
+        if(distanceToWalkPoint.magnitude < 1f)
+        {
+            walkPointSet = false;
+        }
+    }
+    private void SearchWalkPoint()
+    {
+        //Calculate random point in range
+        float randomZ = Random.Range(-walkPointRange, walkPointRange);
+        float randomX = Random.Range(-walkPointRange, walkPointRange);
+
+        walkPoint = new Vector3(transform.position.x + randomX, transform.position.y, transform.position.z + randomZ);
+
+        if (Physics.Raycast(walkPoint, -transform.up, 2f, whatIsGround))
+        {
+            walkPointSet = true;
+        }
+    }
+    private void ChasePlayer()
+    {
+        agent.SetDestination(player.position);
+    }
+    private void AttackPlayer()
+    {
+        //Stop enemy movement
+        agent.SetDestination(transform.position);
+
+        transform.LookAt(player);
+
+        if (!alreadyAttacked)
+        {
+            ///Add Attack Code Here
+            Rigidbody rb = Instantiate(bullet, BulletSpawn.position, transform.rotation).GetComponent<Rigidbody>();
+
+            rb.AddForce(transform.forward * 15f, ForceMode.Impulse);
+            rb.AddForce(transform.up * 2f, ForceMode.Impulse);
+            ///
+
+            alreadyAttacked = true;
+            Invoke(nameof(ResetAttack), timeBetweenAttacks);
+
+            
+        }
+    }
+    private void ResetAttack()
+    {
+        alreadyAttacked = false;
     }
 
     public void takeDamage(int amount)
@@ -46,5 +133,12 @@ public class enemyAI : MonoBehaviour, IDamage
         model.material.color = Color.white;
     }
 
-
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, attackRange);
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireSphere(transform.position, sightRange);
+    }
+   
 }
