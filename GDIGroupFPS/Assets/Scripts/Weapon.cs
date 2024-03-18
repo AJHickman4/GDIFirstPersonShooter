41e151937fa0e62d2daf3bf755aae39580da7e17
Bryan Bechtel <H_hunter19@yahoo.com>
2024-03-18 02:22:02 -0600

    Added ammo count / mag count

--- GDIGroupFPS/Assets/Scripts/Weapon.cs
+++ GDIGroupFPS/Assets/Scripts/Weapon.cs
@@ -1,48 +1,50 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Weapon : MonoBehaviour
 {
-    // Shooting parameters
+    [Header("Shooting Parameters")]
     public bool isShooting;
-    public bool readyToShoot;
-    //private bool allowReset = true;
+    public bool readyToShoot = true;
     public float shootingDelay = 2f;
-
-    // Time tracking for auto firing delay
     private float timeSinceLastShot = 0f;
 
-    // Burst fire parameters
+    [Header("Burst Fire Parameters")]
     public int bulletsPerBurst = 3;
-    private int currentBurst;
 
-    // Spread parameters
+    [Header("Spread Parameters")]
     public float spreadIntensity = 0.1f;
 
-    // Bullet parameters
+    [Header("Bullet Parameters")]
     public GameObject bulletPrefab;
     public Transform bulletSpawn;
     public float bulletVelocity = 30;
     public float bulletPrefabLife = 3f;
+
+    [Header("Camera")]
     public Camera playerCamera;
-    
-    //Shotgun parameters
-    public float pellets = 5;
-    
-    
-    
-    
+
+    [Header("Shotgun Parameters")]
+    public int pellets = 5;
+
+    [Header("Ammo Parameters")]
+    public int ammoPerMag = 30; // Ammo in each magazine
+    public int currentAmmo;
+    public int totalMags = 3; // Total number of magazines you can carry
+    private int currentMags;
+
+    [Header("Damage")]
     public int bulletDamage = 10;
 
+    [Header("Equipment Status")]
     public bool isEquipped = false;
-   
+
     public enum ShootingMode
     {
         Single,
         Burst,
         Auto,
-        shotgun
+        Shotgun
     }
 
     public ShootingMode mode;
@@ -50,128 +52,117 @@ public class Weapon : MonoBehaviour
     void Awake()
     {
         readyToShoot = true;
-        currentBurst = bulletsPerBurst;
+        currentAmmo = ammoPerMag; // Initialize ammo count
+        currentMags = totalMags; // Initialize magazine count
     }
 
     void Update()
     {
-        timeSinceLastShot += Time.deltaTime; // Increment the time since the last shot
+        timeSinceLastShot += Time.deltaTime;
         if (isEquipped)
         {
-            switch (mode)
+            if (Input.GetKeyDown(KeyCode.R) && currentMags > 0 && currentAmmo < ammoPerMag)
             {
-                case ShootingMode.Single:
-                    if (Input.GetButtonDown("Shoot") && readyToShoot)
-                    {
-                        FireWeapon();
-                    }
-                    break;
-                case ShootingMode.Burst:
-                    if (Input.GetButtonDown("Shoot") && readyToShoot)
-                    {
-                        StartCoroutine(FireBurst());
-                    }
-                    break;
-                case ShootingMode.Auto:
-                    if (Input.GetButton("Shoot") && readyToShoot && timeSinceLastShot >= shootingDelay)
-                    {
-                        FireWeapon();
-                    }
-                    break;
-                    case ShootingMode.shotgun:
-                    if (Input.GetButtonDown("Shoot") && readyToShoot)
-                    {
-                        StartCoroutine(FireShotgun());
-                    }
-                    break;
-            
+                StartCoroutine(Reload());
+            }
+            else if (readyToShoot && currentAmmo > 0)
+            {
+                switch (mode)
+                {
+                    case ShootingMode.Single:
+                        if (Input.GetButtonDown("Shoot"))
+                        {
+                            FireWeapon();
+                        }
+                        break;
+                    case ShootingMode.Burst:
+                        if (Input.GetButtonDown("Shoot"))
+                        {
+                            StartCoroutine(FireBurst());
+                        }
+                        break;
+                    case ShootingMode.Auto:
+                        if (Input.GetButton("Shoot") && timeSinceLastShot >= shootingDelay)
+                        {
+                            FireWeapon();
+                        }
+                        break;
+                    case ShootingMode.Shotgun:
+                        if (Input.GetButtonDown("Shoot"))
+                        {
+                            StartCoroutine(FireShotgun());
+                        }
+                        break;
+                }
             }
         }
     }
 
     private void FireWeapon()
     {
-        if (!readyToShoot) return;
-
-        readyToShoot = false;
         ShootBullet();
-        //allowReset = false;
-
-        if (mode == ShootingMode.Auto)
+        currentAmmo--;
+        timeSinceLastShot = 0f; // Reset timer for auto and single shot mode
+        if (mode != ShootingMode.Auto) // For single shot, reset immediately
         {
-            timeSinceLastShot = 0f; // Reset the timer for auto mode
+            readyToShoot = false;
+            StartCoroutine(ResetShot(shootingDelay)); // Add delay after firing
         }
-
-        if (mode != ShootingMode.Burst)
-        {
-            StartCoroutine(ResetShot());
-        }
-    }
-
-    void ShootBullet()
-    {
-        Vector3 shootingDirection = CalculateDirectionAndSpread().normalized;
-        GameObject bullet = Instantiate(bulletPrefab, bulletSpawn.position, Quaternion.LookRotation(shootingDirection));      
-        bullet.GetComponent<Rigidbody>().AddForce(shootingDirection * bulletVelocity, ForceMode.Impulse);
-        bullet.GetComponent<Bullet>().SetDamage(bulletDamage);
-        StartCoroutine(DestroyBulletAfterTime(bullet, bulletPrefabLife));
-    }
-
-    IEnumerator DestroyBulletAfterTime(GameObject bullet, float delay)
-    {
-        yield return new WaitForSeconds(delay);
-        Destroy(bullet);
     }
 
     IEnumerator FireBurst()
     {
-       // allowReset = false;
-        for (int i = 0; i < bulletsPerBurst; i++)
+        readyToShoot = false;
+        for (int i = 0; i < bulletsPerBurst && currentAmmo > 0; i++)
         {
-            if (readyToShoot)
-            {
-                ShootBullet();
-                yield return new WaitForSeconds(shootingDelay / bulletsPerBurst);
-            }
+            ShootBullet();
+            currentAmmo--;
+            yield return new WaitForSeconds(shootingDelay / bulletsPerBurst);
         }
-        StartCoroutine(ResetShot());
+        StartCoroutine(ResetShot(shootingDelay));
     }
 
-    IEnumerator ResetShot()
+    IEnumerator FireShotgun()
     {
-        
-        yield return new WaitForSeconds(shootingDelay);
+        readyToShoot = false;
+        for (int i = 0; i < pellets && currentAmmo > 0; i++)
+        {
+            ShootBullet();
+            currentAmmo--;
+            yield return new WaitForSeconds(shootingDelay / pellets);
+        }
+        StartCoroutine(ResetShot(shootingDelay));
+    }
+
+    IEnumerator Reload()
+    {
+        yield return new WaitForSeconds(1f); // Reload time
+        currentMags--;
+        currentAmmo = ammoPerMag;
+    }
+
+    void ShootBullet()
+    {
+        Vector3 shootingDirection = CalculateDirectionAndSpread();
+        GameObject bullet = Instantiate(bulletPrefab, bulletSpawn.position, Quaternion.LookRotation(shootingDirection));
+        bullet.GetComponent<Rigidbody>().AddForce(shootingDirection * bulletVelocity, ForceMode.Impulse);
+        Destroy(bullet, bulletPrefabLife);
+    }
+
+    IEnumerator ResetShot(float delay)
+    {
+        yield return new WaitForSeconds(delay);
         readyToShoot = true;
-        //allowReset = true;
-        currentBurst = bulletsPerBurst; // Reset burst count after a delay
     }
 
     public Vector3 CalculateDirectionAndSpread()
     {
         Vector3 spread = new Vector3(Random.Range(-spreadIntensity, spreadIntensity), Random.Range(-spreadIntensity, spreadIntensity), 0);
-        Vector3 shootingDirection = playerCamera.transform.forward + spread; // Add spread to the forward direction
-        return shootingDirection;
+        return playerCamera.transform.forward + spread;
     }
 
     public void SetEquipped(bool equipped)
     {
         isEquipped = equipped;
-    }
-
-    IEnumerator FireShotgun()
-    {
-        
-        //allowReset = false;
-        int pellets = 5;
-        for (int i = 0; i < pellets; i++)
-        {
-            if (readyToShoot)
-            {
-                ShootBullet();
-                yield return new WaitForSeconds(shootingDelay / pellets);
-            }
-        }
-        
-        StartCoroutine(ResetShot());
     }
 }
