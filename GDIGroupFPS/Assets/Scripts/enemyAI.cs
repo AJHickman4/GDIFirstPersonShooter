3c489e350156da8e4bdacca53fa108ee1162b478
AJHickman4 <ajhickman@student.fullsail.edu>
2024-03-21 17:29:11 -0500

    Testing new enemy model, updated AI

--- GDIGroupFPS/Assets/Scripts/enemyAI.cs
+++ GDIGroupFPS/Assets/Scripts/enemyAI.cs
@@ -8,52 +8,137 @@ public class enemyAI : MonoBehaviour, IDamage
 {
     [SerializeField] Renderer model;
     [SerializeField] NavMeshAgent agent;
+    [SerializeField] Animator anim;
     [SerializeField] Transform shootPos;
+    [SerializeField] Transform headPos;
 
     [SerializeField] int HP;
     [SerializeField] int speed;
+    [SerializeField] int viewCone;
+    [SerializeField] int faceTargetSpeed;
+    [SerializeField] float animSpeedTrans;
+    [SerializeField] int roamDist;
+    [SerializeField] int roamPauseTime;
 
     [SerializeField] GameObject bullet;
     [SerializeField] float shootRate;
 
     bool isShooting;
     bool playerInRange;
+    float angleToPlayer;
+    Vector3 playerDirection;
+    float stoppingDistOrg;
+    Vector3 startingPos;
+    bool destinationChosen;
+
 
     void Start()
     {
-        
+        stoppingDistOrg = agent.stoppingDistance;
+        agent.stoppingDistance = 0;
+        startingPos = transform.position;
     }
 
     void Update()
     {
-        if (playerInRange)
-        {
-            agent.SetDestination(gameManager.instance.player.transform.position);
+        float animSpeed = agent.velocity.normalized.magnitude;
 
-            if (!isShooting)
+        anim.SetFloat("Speed", Mathf.Lerp(anim.GetFloat("Speed"), animSpeed, Time.deltaTime * animSpeedTrans));
+
+        if (playerInRange && !canSeePlayer())
+        {
+            StartCoroutine(Roam()); //If the player is in range but cannot be seen, AI should roam
+        }
+        else if (!playerInRange)
+        {
+            StartCoroutine(Roam()); //AI should roam if player isn't in range
+        }
+    }
+
+    IEnumerator Roam()
+    {
+        if (agent.remainingDistance < 0.05f && !destinationChosen)
+        {
+            destinationChosen = true;
+            agent.stoppingDistance = 0;
+            yield return new WaitForSeconds(roamPauseTime);
+
+            Vector3 randomPos = Random.insideUnitSphere * roamDist;
+            randomPos += startingPos;
+
+            NavMeshHit hit;
+            NavMesh.SamplePosition(randomPos, out hit, roamDist, 1);
+            agent.SetDestination(hit.position);
+
+            destinationChosen = false;
+        }
+    }
+
+    bool canSeePlayer()
+    {
+        playerDirection = gameManager.instance.player.transform.position - headPos.position;
+        angleToPlayer = Vector3.Angle(playerDirection, transform.forward);
+        Debug.Log(angleToPlayer);
+        Debug.DrawRay(headPos.position, playerDirection, Color.yellow);
+
+        RaycastHit hit;
+        if (Physics.Raycast(headPos.position, playerDirection, out hit))
+        {
+            Debug.Log(hit.collider.name);
+            if (hit.collider.CompareTag("Player") && angleToPlayer <= viewCone)
             {
-                StartCoroutine(shoot());
+                agent.stoppingDistance = stoppingDistOrg;
+                agent.SetDestination(gameManager.instance.player.transform.position);
+
+                if (!isShooting)
+                {
+                    StartCoroutine(shoot());
+                }
+
+                if (agent.remainingDistance <= agent.stoppingDistance)
+                {
+                    faceTarget();
+                }
+
+                return true;
             }
         }
+
+        agent.stoppingDistance = 0;
+        return false;
+    }
+
+    //Try IK to move pelvis only
+    void faceTarget()
+    {
+        Quaternion rot = Quaternion.LookRotation(new Vector3(playerDirection.x, transform.position.y, playerDirection.z));
+        transform.rotation = Quaternion.Lerp(transform.rotation, rot, Time.deltaTime * faceTargetSpeed);
+
     }
 
     IEnumerator shoot()
     {
         isShooting = true;
-        Instantiate(bullet, shootPos.position, transform.rotation);
+        anim.SetTrigger("Shoot");
 
         yield return new WaitForSeconds(shootRate);
         isShooting = false;
     }
 
+    public void createBullet()
+    {
+        Instantiate(bullet, shootPos.position, transform.rotation);
+    }
     public void takeDamage(int amount)
     {
         HP -= amount;
+        anim.SetTrigger("Damage");
+        agent.SetDestination(gameManager.instance.player.transform.position);
         StartCoroutine(flashRed());
 
         if (HP <= 0)
         {
-           Destroy(gameObject);
+            Destroy(gameObject);
         }
     }
 
@@ -77,6 +162,7 @@ public class enemyAI : MonoBehaviour, IDamage
         if (other.CompareTag("Player"))
         {
             playerInRange = false;
+            agent.stoppingDistance = 0;
         }
     }
 }
