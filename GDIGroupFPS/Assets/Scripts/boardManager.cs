63c132cf60031ca5bc5a1c99f3ec13a031bff78e
amabunada <137794655+amabunada@users.noreply.github.com>
2024-03-24 22:02:42 -0400

    Added better Key UI + Board mechanics

--- GDIGroupFPS/Assets/Scripts/boardManager.cs
+++ GDIGroupFPS/Assets/Scripts/boardManager.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class boardManager : MonoBehaviour
@@ -7,33 +8,80 @@ public class boardManager : MonoBehaviour
     [SerializeField] List<GameObject> boards;
     [SerializeField] int boardFixSpeed;
     [SerializeField] int boardDestroySpeed;
+    [SerializeField] GameObject player;
     public bool fixing;
+    public bool destroy;
+
+    bool enemyTrigger;
+    bool playerTrigger;
 
     // Start is called before the first frame update
     void Start()
     {
-
+        player = GameObject.FindWithTag("Player");
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        
     }
-    private void OnTriggerStay(Collider other)
+    //private void OnTriggerStay(Collider other)
+    //{
+
+    //    if (other.CompareTag("Player"))
+    //        playerTrigger = true;
+    //   else 
+    //        enemyTrigger = true;
+
+
+    //            if (Input.GetKey(KeyCode.E))
+    //                fixing = true;
+    //            else if (other.CompareTag("Player"))
+    //                fixing = false;
+    //            if (fixing)
+    //                StartCoroutine(fixBoards());
+
+
+    //    if (enemyTrigger)
+    //    {
+    //        destroy = true;
+    //        StartCoroutine(destroyBoards());
+    //    }
+    //}
+
+    private void OnTriggerEnter(Collider other)
     {
-        if (other.CompareTag("Player"))
+        if (other.name == "Player")
+            playerTrigger = true;
+        if (other.tag == "Enemy")
+            enemyTrigger = true;
+
+        if (playerTrigger)
         {
-            if (Input.GetKey(KeyCode.E))
-                fixing = true;
-            else if (other.CompareTag("Player"))
-                fixing = false;
+            fixing = true;
             if (fixing)
                 StartCoroutine(fixBoards());
         }
-        if (other.CompareTag("Enemy"))
+        if (enemyTrigger)
         {
-            StartCoroutine(fixBoards());
+            destroy = true;
+            if (destroy)
+                StartCoroutine(destroyBoards());
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.name == "Player")
+        {
+            playerTrigger = false;
+            fixing = false;
+        }
+        if (other.tag == "Enemy")
+        { 
+            enemyTrigger = false;
+            destroy = false;
         }
     }
 
@@ -60,10 +108,8 @@ public class boardManager : MonoBehaviour
     {
         for (int i = 0; i < boards.Count; i++)
         {
-
             yield return new WaitForSeconds(boardDestroySpeed);
             boards[i].SetActive(false);
-
         }
     }
 }
