cfe67c53e1239c8389572076591d01930a2577da
amabunada <137794655+amabunada@users.noreply.github.com>
2024-04-19 11:01:11 -0400

    Applied animated buttons to all UI screens (pause, win, lose). Edited win screen. Applied sounds to those menus

--- GDIGroupFPS/Assets/Scripts/buttonFunctions.cs
+++ GDIGroupFPS/Assets/Scripts/buttonFunctions.cs
@@ -5,26 +5,32 @@ using UnityEngine.SceneManagement;
 
 public class buttonFunctions : MonoBehaviour
 {
+
+    [SerializeField] AudioSource aud;
+
     public void resume()
     {
+        aud.Play();
         gameManager.instance.stateUnPaused();
     }
 
     public void restart()
     {
-       
+        aud.Play(); 
         SceneManager.LoadScene(SceneManager.GetActiveScene().name);
         gameManager.instance.stateUnPaused();
     }
     
     public void respawn()
     {
+        aud.Play();
         gameManager.instance.stateUnPaused();
         gameManager.instance.playerScript.spawnPlayer();
     }
 
     public void quit()
     {
+        aud.Play();
 #if UNITY_EDITOR
         UnityEditor.EditorApplication.isPlaying = false;
 #else
