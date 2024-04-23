66dffd66bcd91dad398276cb6ab766646b2d3e1c
Bryan Bechtel <H_hunter19@yahoo.com>
2024-04-22 21:04:25 -0600

    glow

--- GDIGroupFPS/Assets/Scripts/Grenade.cs
+++ GDIGroupFPS/Assets/Scripts/Grenade.cs
@@ -3,13 +3,17 @@ using UnityEngine;
 
 public class Grenade : MonoBehaviour
 {
-    [SerializeField] private float delay = 3.0f; 
-    [SerializeField] private float blastRadius = 5.0f; 
-    [SerializeField] private int damage = 50;  
-    [SerializeField] private LayerMask damageLayer;  
-    [SerializeField] private GameObject explosionEffect; 
+    [SerializeField] private float delay = 3.0f;
+    [SerializeField] private float blastRadius = 5.0f;
+    [SerializeField] private int damage = 50;
+    [SerializeField] private LayerMask damageLayer;
+    [SerializeField] private GameObject explosionEffect;
     [SerializeField] private AudioClip explosionSound;
-    [SerializeField] private AudioSource audioSource; 
+    [SerializeField] private AudioSource audioSource;
+    [SerializeField] private Material defaultMaterial;
+    [SerializeField] private Material flashRedMaterial;
+
+    public Renderer rend;  
 
     private float countdown;
     private bool hasExploded = false;
@@ -19,6 +23,7 @@ public class Grenade : MonoBehaviour
     {
         countdown = delay;
         audioSource = GetComponent<AudioSource>();
+        SetMaterial(defaultMaterial); 
     }
 
     void Update()
@@ -26,6 +31,7 @@ public class Grenade : MonoBehaviour
         countdown -= Time.deltaTime;
         if (countdown <= 0.5f && !soundPlayed)
         {
+            SetMaterial(flashRedMaterial);
             audioSource.PlayOneShot(explosionSound);
             soundPlayed = true;
         }
@@ -42,6 +48,7 @@ public class Grenade : MonoBehaviour
         {
             Instantiate(explosionEffect, transform.position, transform.rotation);
         }
+
         Collider[] colliders = Physics.OverlapSphere(transform.position, blastRadius, damageLayer);
         foreach (Collider nearbyObject in colliders)
         {
@@ -54,8 +61,18 @@ public class Grenade : MonoBehaviour
                 }
             }
         }
-        float destroyDelay = Mathf.Max(0.5f + explosionSound.length, 0f); 
+
+        float destroyDelay = Mathf.Max(0.5f + explosionSound.length, 0f);
         Destroy(gameObject, destroyDelay);
     }
-}
 
+    private void SetMaterial(Material mat)
+    {
+        Material[] materials = rend.materials;
+        for (int i = 0; i < materials.Length; i++)
+        {
+            materials[i] = mat;
+        }
+        rend.materials = materials;
+    }
+}
