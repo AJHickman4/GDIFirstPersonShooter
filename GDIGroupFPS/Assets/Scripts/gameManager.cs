86c7292e37f0436a4293420dbbb5849cab93f7fd
Bryan Bechtel <H_hunter19@yahoo.com>
2024-04-23 22:42:24 -0600

    dash

--- GDIGroupFPS/Assets/Scripts/gameManager.cs
+++ GDIGroupFPS/Assets/Scripts/gameManager.cs
@@ -276,7 +276,7 @@ public class gameManager : MonoBehaviour
     {
         
         { 
-            isResetting = true;
+
             playerScript.controller.enabled = false;
             yield return new WaitForSeconds(0.5f);
             player.transform.position = startingSpawn.transform.position;
